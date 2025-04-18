b409eea537c383fa24ea2fa51f2ed6e711b85737
Dr Dennis Evangelista <devangel77b@gmail.com>
2025-04-18 09:10:59 -0400

    blah

--- ear.py
+++ ear.py
@@ -1,28 +1,39 @@
 #!/bin/env python
 
-class Vertex():
+class Vertex(object):
     def __init__(self,x=None):
         self.x = None
         return
 
-class Edge():
+class HalfEdge(object):
+    def __init__(self):
+        self.v = None
+        self.f = None
+        self.nexth = None
+        self.twinh = None
+        return
+
+class Edge(object):
     def __init__(self,uv=None,assignment=None):
+        self.h = None
         self.assignment = assignment
-        self.vertices = uv
-        self.faces = None
         return
 
-class Face():
-    def __init__(self):
-        self.vertices = None
-        self.edges = None
+class Face(object):
+    def __init__(self,h=None):
+        self.h = h
         return
 
-class CreasePattern():
+class GenericHEDS(object):
     def __init__(self):
         self.vertices = None
         self.edges = None
         self.faces = None
+        return
+    
+class CreasePattern(GenericHEDS):
+    def __init__(self):
+        super.__init__(self)
         return
     
     
