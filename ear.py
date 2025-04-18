8cb73f1d5f5a27c5b30fea1081c0b65668370b4e
Dr Dennis Evangelista <devangel77b@gmail.com>
2025-04-18 12:52:33 -0400

    blah

--- ear.py
+++ ear.py
@@ -1,40 +1,86 @@
 #!/bin/env python
 
+import json
+
 class Vertex(object):
     def __init__(self,x=None):
-        self.x = None
+        self.x = x
+        self.index = None
         return
-
-class HalfEdge(object):
-    def __init__(self):
-        self.v = None
-        self.f = None
-        self.nexth = None
-        self.twinh = None
-        return
-
+    
 class Edge(object):
-    def __init__(self,uv=None,assignment=None):
-        self.h = None
-        self.assignment = assignment
+    def __init__(self):
+        self.vertices = None
+        self.assignment = None
+        self.index = None
         return
 
 class Face(object):
-    def __init__(self,h=None):
-        self.h = h
+    def __init__(self):
+        self.edges = None
+        self.index = None
         return
 
-class GenericHEDS(object):
+class Surface(object):
     def __init__(self):
-        self.vertices = None
-        self.edges = None
-        self.faces = None
+        self.vertices = list()
+        self.edges = list()
+        self.faces = list()
         return
     
-class CreasePattern(GenericHEDS):
-    def __init__(self):
-        super.__init__(self)
-        return
+    def add_vertex(self,x):
+        result = Vertex(x)
+        self.vertices.append(result)
+        result.index = self.vertices.index(result)
+        return result
+
+    def add_edge_by_indices(self,v):
+        result = Edge()
+        result.vertices = v
+        self.edges.append(result)
+        result.index = self.edges.index(result)
+        return result
+    
+    def add_face_by_indices(self,v):
+        result = Face()
+        result.vertices = v
+        self.faces.append(result)
+        result.index = self.faces.index(result)
+        return result
+    
+    def as_dict(self):
+        result = dict()
+        result['vertices_coords']=list()
+        for v in self.vertices:
+            result['vertices_coords'].append(v.x)
+        result['edges_vertices']=list()
+        for e in self.edges:
+            result['edges_vertices'].append(e.vertices)
+        result['faces_vertices']=list()
+        for f in self.faces:
+            result['faces_vertices'].append(f.vertices)
+        return result
+
+
+
+
+        
+
+
+def from_fold(filename):
+    handle = open(filename,'r')
+    json_dict = json.load(handle)
+    handle.close()
+    result = Surface()
+    for v in json_dict['vertices_coords']:
+        result.add_vertex(v)
+    for e in json_dict['edges_vertices']:
+        result.add_edge_by_indices(e)
+    for f in json_dict['faces_vertices']:
+        result.add_face_by_indices(f)
+    return result
+
+    
     
     
 
